c46377fee71b2922b590e26e1f094983d56441c0
Animus1017 <slayergaming2k20@gmail.com>
2025-03-19 03:41:27 +0530

    cors commit

--- backend/server.js
+++ backend/server.js
@@ -21,7 +21,10 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: [
+      "https://studynotion-mi17.vercel.app",
+      "https://studynotion-mi17.vercel.app/",
+    ],
     credentials: true,
   })
 );
